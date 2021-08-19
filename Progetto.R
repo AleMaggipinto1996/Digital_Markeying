38870029bc51467584b2ccc96f3f07cdaab12860
AleMaggipinto1996 <74192743+AleMaggipinto1996@users.noreply.github.com>
2021-08-19 13:22:57 +0200

    Update Progetto.R

--- Progetto.R
+++ Progetto.R
@@ -11,7 +11,16 @@ library(wesanderson)
 
 ### Importazione dei Dataset
 
-raw_1_cli_fid <- read.csv("~/Documents/GitHub/Progetto_DigitalM/DMktg_DSLab_Data_1/raw_1_cli_fid.csv", sep=";", na.strings = c("NA", ""))
-df_2_cli_account <- read.csv("~/Documents/GitHub/Progetto_DigitalM/DMktg_DSLab_Data_1/raw_2_cli_account.csv", sep=";", na.strings = c("NA", ""))
-df_3_cli_address <- read.csv("~/Documents/GitHub/Progetto_DigitalM/DMktg_DSLab_Data_1/raw_3_cli_address.csv", sep=";", na.strings = c(""))
-df_4_cli_privacy <- read.csv("~/Documents/GitHub/Progetto_DigitalM/DMktg_DSLab_Data_1/raw_4_cli_privacy.csv", sep=";", na.strings = c("NA", ""))
+raw_1_cli_fid <- read.csv2("~/Documents/DMktg_DSLab_Data_1/raw_1_cli_fid.csv", sep=";", na.strings = c("NA", ""))
+
+df_2_cli_account <- read.csv2("~/Documents/DMktg_DSLab_Data_1/raw_2_cli_account.csv", sep=";", na.strings = c("NA", ""))
+
+df_3_cli_address <- read.csv2("~/Documents/DMktg_DSLab_Data_1/raw_3_cli_address.csv", sep=";", na.strings = c(""))
+
+df_4_cli_privacy <- read.csv2("~/Documents/DMktg_DSLab_Data_1/raw_4_cli_privacy.csv", sep=";", na.strings = c("NA", ""))
+
+df_5_camp_cat <- read.csv2("~/Documents/DMktg_DSLab_Data_1/raw_5_camp_cat.csv", sep=";", na.strings = c("NA", ""))
+
+df_6_camp_event <- read.csv2("~/Documents/DMktg_DSLab_Data_1/raw_6_camp_event.csv", sep=";", na.strings = c("NA", ""))
+
+df_7_tic <- read.csv2("~/Documents/DMktg_DSLab_Data_1/raw_7_tic.csv", na.strings = c("NA", ""), stringsAsFactors = FALSE)
