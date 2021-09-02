52fe690b891f1ca42947316aa8df02c684c51d7d
AleMaggipinto1996 <74192743+AleMaggipinto1996@users.noreply.github.com>
2021-09-02 15:44:44 +0200

    Update Progetto.R

--- Progetto.R
+++ Progetto.R
@@ -746,8 +746,67 @@ cons_idaddress_df2_df3
 
 #### EXPLORE COLUMNS of df_3 ####
 
-#### ???? TO DO df_3 ???? ####
 # EXPLORE the df_3_cli_address_clean relevant variables
+
+### Variabile REGION ###
+
+df_3_cli_address_clean_region_distrib <- df_3_cli_address_clean %>%
+  group_by(REGION) %>%
+  dplyr::summarize(TOT_IDs = n_distinct(ID_ADDRESS)) %>%
+  mutate(PERCENT = TOT_IDs/sum(TOT_IDs)) %>%
+  arrange(desc(PERCENT))
+
+df_3_cli_address_clean_region_distrib
+
+## plot distribution
+plot_df_3_cli_address_clean_region_distrib <- (
+  ggplot(data=df_3_cli_address_clean_region_distrib
+         , aes(x=REGION, y=PERCENT)) +
+    geom_bar(stat="identity"
+             , fill="steelblue") +
+    theme_minimal()
+)
+
+plot_df_3_cli_address_clean_region_distrib
+
+#soprattutto la Lombardia
+
+### variabile PROVINCIA ###
+
+## compute distribution
+df_3_cli_address_clean_prv_distrib <- df_3_cli_address_clean %>%
+  group_by(PRV) %>%
+  dplyr::summarize(TOT_IDs = n_distinct(ID_ADDRESS)) %>%
+  mutate(PERCENT = TOT_IDs/sum(TOT_IDs)) %>%
+  arrange(desc(PERCENT))
+
+df_3_cli_address_clean_prv_distrib
+
+## plot distribution
+plot_df_3_cli_address_clean_prv_distrib <- (
+  ggplot(data=df_3_cli_address_clean_prv_distrib
+         , aes(x=PRV, y=PERCENT)) +
+    geom_bar(stat="identity"
+             , fill="steelblue") +
+    theme_minimal()
+)
+
+plot_df_3_cli_address_clean_prv_distrib
+
+# troppe variabili non è significativo
+
+### variabile CAP ###
+
+## compute distribution
+df_3_cli_address_clean_cap_distrib <- df_3_cli_address_clean %>%
+  group_by(CAP) %>%
+  dplyr::summarize(TOT_IDs = n_distinct(ID_ADDRESS)) %>%
+  mutate(PERCENT = TOT_IDs/sum(TOT_IDs)) %>%
+  arrange(desc(PERCENT))
+
+df_3_cli_address_clean_cap_distrib
+
+#anche i CAP ci sono troppe variabili non è utile
 
 #### FINAL REVIEW df_3_clean ####
 
