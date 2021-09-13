9f853d93414284e919ce9a6273929b0a25b2419a
Matteo S <m.sala137@campus.unimib.it>
2021-09-13 12:19:38 +0200

    Update C01_preparation_df1_aziende.R

--- Clienti Business/C01_preparation_df1_aziende.R
+++ Clienti Business/C01_preparation_df1_aziende.R
@@ -1,170 +1,6 @@
-#### PREPARAZIONE DEI DATASET ####
-
-### Dataset n?1 ###
-
-###primo sguardo al dataset
-
-str(df_1_cli_fid)  #come sono fatti i nostri dati
-summary(df_1_cli_fid)
-
-#si vede che la maggior parte dei clienti hanno un account ed è attivo
-
-## START CLEANING df_1 ##
-
-### Ricreare il dataset
-
-df_1_cli_fid_clean <- df_1_cli_fid
-
-### Check for duplicates (non duplicati per CLI-FID)
-df_1_cli_fid_clean %>% summarize(TOT_ID_CLIs = n_distinct(ID_CLI)
-                                 , TOT_ID_FIDs = n_distinct(ID_FID)
-                                 , TOT_ID_CLIFIDs = n_distinct(paste0(as.character(ID_CLI),"-",as.character(ID_FID)))
-                                 , TOT_ROWs = n())
-
-#ci sono più registrazioni di carte fedeltà di ciascun cliente e questo non ci sorprende
-
-### Formattazione delle date 
-df_1_cli_fid_clean <- df_1_cli_fid_clean %>%
-  mutate(DT_ACTIVE = as.Date(DT_ACTIVE))
-
-### Formattazione boleani in fattori ##
-df_1_cli_fid_clean <- df_1_cli_fid_clean %>%
-  mutate(TYP_CLI_FID = as.factor(TYP_CLI_FID)) %>%
-  mutate(STATUS_FID = as.factor(STATUS_FID))
-
-### Numero di programmi fedelt? per numero di clienti ##
-##quante sottoscrizioni ho per ciascun cliente?
-
-num_fid_x_cli <- df_1_cli_fid_clean %>%
-  group_by(ID_CLI) %>%
-  summarize(NUM_FIDs =  n_distinct(ID_FID)
-            , NUM_DATEs = n_distinct(DT_ACTIVE)
-  )
-
-#Controllando le date, emerge che vi sono clienti hanno probabilmente sbagliato/cambiato la fidelizzazione pi? volte
-#nello stesso giorno
-
-tot_id_cli <- n_distinct(num_fid_x_cli$ID_CLI)
-
-## compute the distribution of number of subscriptions
-dist_num_fid_x_cli <- num_fid_x_cli %>%
-  group_by(NUM_FIDs, NUM_DATEs) %>%
-  summarize(TOT_CLIs = n_distinct(ID_CLI)) %>%
-  mutate(PERCENT_CLIs = TOT_CLIs/tot_id_cli)
-
-dist_num_fid_x_cli
-
-## Ci sono clienti con molteplici programmi fedelt? ##
-# let examine in details clients with multiple subscriptions#
-
-num_fid_x_cli %>% filter(NUM_FIDs == 3)
-
-#andiamo a vedere nel dettaglio per esempio un cliente#
-# each subscription can have different dates
-df_1_cli_fid %>% filter(ID_CLI == 621814)
-# there could be subscriptions at the same dates [possibly for technical reasons]
-df_1_cli_fid %>% filter(ID_CLI == 320880)
-
-#### RESHAPING df_1 ####
-
-## combining information ##
-
-# from first subscription  --> registration date, store for registration
-# from last subscription   --> type of fidelity, status
-# from subscriptions count --> number of subscriptions made
-
-#ID 1 ? l'ID relativo alla registrazione online, gli altri sono negozi fisici
-
-
-df_1_cli_fid_first <- df_1_cli_fid_clean %>%
-  group_by(ID_CLI) %>%
-  filter(DT_ACTIVE == min(DT_ACTIVE)) %>%
-  arrange(ID_FID) %>%
-  filter(row_number() == 1) %>%
-  ungroup() %>%
-  as.data.frame()
-
-df_1_cli_fid_last <- df_1_cli_fid_clean %>%
-  group_by(ID_CLI) %>%
-  filter(DT_ACTIVE == max(DT_ACTIVE)) %>%
-  arrange(desc(ID_FID)) %>%
-  filter(row_number() == 1) %>%
-  ungroup() %>%
-  as.data.frame()
-
-#ricombinare le informazioni per avere un dataset completo
-
-df_1_cli_fid_clean <- df_1_cli_fid_last %>%
-  select(ID_CLI
-         , ID_FID
-         , LAST_COD_FID = COD_FID
-         , LAST_TYP_CLI_FID = TYP_CLI_FID
-         , LAST_STATUS_FID = STATUS_FID
-         , LAST_DT_ACTIVE = DT_ACTIVE) %>%
-  left_join(df_1_cli_fid_first %>%
-              select(ID_CLI
-                     , FIRST_ID_NEG = ID_NEG
-                     , FIRST_DT_ACTIVE = DT_ACTIVE)
-            , by = 'ID_CLI') %>%
-  left_join(num_fid_x_cli %>%
-              select(ID_CLI
-                     , NUM_FIDs) %>%
-              mutate(NUM_FIDs = as.factor(NUM_FIDs))
-            , by = 'ID_CLI')
-
-### variabile Registrazione Online / Negozio Fisico ###
-
-#creo una nuova colonna 0/1: ? 1 se si tratta di registrazione online, 
-#? 0 se ? stata fatta nel negozio
-
-RegOnline <- as.data.frame(df_1_cli_fid_clean$FIRST_ID_NEG)
-colnames(RegOnline)<- "RegOnline"
-
-RegOnline <- RegOnline %>% mutate(RegOnline = as.factor(if_else(RegOnline!= 1, "0", as.character(RegOnline))))
-
-df_1_cli_fid_clean$RegOnline <- RegOnline$RegOnline
-
-
-## EXPLORE COLUMNS of df_1 ##
-
-### variable LAST_COD_FID ###
-
-## come si distribuiscono i nostri clienti
-df1_dist_codfid <- df_1_cli_fid_clean %>%
-  group_by(LAST_COD_FID) %>%
-  summarize(TOT_CLIs = n_distinct(ID_CLI)) %>%
-  mutate(PERCENT = TOT_CLIs/sum(TOT_CLIs)) %>%
-  arrange(desc(PERCENT))
-
-df1_dist_codfid
-
-## plot distribution
-plot_df1_dist_codfid <- (
-  ggplot(data=df1_dist_codfid
-         , aes(x=LAST_COD_FID, y=TOT_CLIs)
-  ) +
-    geom_bar(stat="identity"
-             , fill="steelblue") +
-    theme_minimal()
-)
-
-plot_df1_dist_codfid
-
-#si vede una sproporzione elevata tra quelli che hanno
-#una fidelizzazione standard e quelli premium
-
-#biz vuol dire categorizzazione tra clienti business e non
-#ovvero clienti e aziende vere e proprie
-
-#? importante capire il modo migliore per presentare i dati
-
-##### SEPARIAMO IN DUE DATASET: AZIENDE/PERSONE ####
 
 df_1_aziende <- df_1_cli_fid_clean %>%  
   filter(LAST_COD_FID == 'PREMIUM BIZ' | LAST_COD_FID == 'STANDARD BIZ')
-
-df_1_persone <- df_1_cli_fid_clean %>%  
-  filter(LAST_COD_FID == 'PREMIUM' | LAST_COD_FID == 'STANDARD')
 
 #_________________________________________
 
@@ -330,7 +166,7 @@ plot_df1_az_codfid_n <- (
 
 plot_df1_az_codfid_n
 
-### variable LAST_TYP_CLI_FID ???? non abbiamo capito che vuol dire ###
+### variable LAST_TYP_CLI_FID ###
 
 ## compute distribution
 df1_az_codfid_main <- df_1_aziende %>%
@@ -406,8 +242,8 @@ plot_dist_df1_status <- ggplot(dist_df1_status,aes(x=LAST_STATUS_FID,y=TOT_CLIEN
 
 plot_dist_df1_status
 
-##Distribuzione dei clienti sulla base della TIPO DI TESSERA FEDELT?
-##0 se ? un account secondario, 1 se ? l'account principale
+##Distribuzione dei clienti sulla base della TIPO DI TESSERA FEDELTA'
+##0 se e' un account secondario, 1 se e' l'account principale
 
 dist_df1_typfid <- df_1_cli_fid_clean %>% group_by(LAST_TYP_CLI_FID) %>%
   summarise(TOT_CLIENTI = n_distinct(ID_CLI)) %>%
@@ -415,8 +251,8 @@ dist_df1_typfid <- df_1_cli_fid_clean %>% group_by(LAST_TYP_CLI_FID) %>%
 
 dist_df1_typfid
 #L'ultima tessera fedelt? attivata dai clienti nel 98.5% dei casi
-#? una tessera principale (363862 casi totali), mentre nell'1.5% dei casi
-#(5610 casi totali) abbiamo che la tessera attivata ? secondaria
+#e' una tessera principale (363862 casi totali), mentre nell'1.5% dei casi
+#(5610 casi totali) abbiamo che la tessera attivata e' secondaria
 
 plot_dist_df1_typfid <- ggplot(dist_df1_typfid,aes(x=LAST_TYP_CLI_FID,y=TOT_CLIENTI)) +
   xlab("TESSERE NON PRINCIPALI/PRINCIPALI") + theme_minimal() + 
@@ -427,7 +263,7 @@ plot_dist_df1_typfid
 
 ### Variabile LAST_DT_ACTIVE###
 ##Consideriamo la variabile LAST_DT_ACTIVE, corrispondente al giorno in cui
-##un cliente ha attivato la sua ultima tessera fedelt?.
+##un cliente ha attivato la sua ultima tessera fedelta'.
 ##Le attivazioni sono state raggruppate in base ai mesi.
 
 dist_df1_active_by_month <- df_1_cli_fid_clean %>%
