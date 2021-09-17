fff550332a2d029bb43c57a45e4d70cd894a2779
AleMaggipinto1996 <74192743+AleMaggipinto1996@users.noreply.github.com>
2021-09-17 12:59:11 +0200

    Update RFM_aziende.R

--- RFM_aziende.R
+++ RFM_aziende.R
@@ -329,6 +329,7 @@ New_class_az <- RFM_2_aziende %>% mutate(NUOVA_CLASSE = CLASSI_2) %>% select(ID_
 
 Confronto_RFM_az <- New_class_az %>% left_join(Old_class_az, by= "ID_CLI")
 Confronto_RFM_az <- Confronto_RFM_az%>% mutate(VECCHIA_CLASSE = fct_explicit_na(VECCHIA_CLASSE,"Clienti_futuri"))
+Confronto_RFM_az <- Confronto_RFM_az%>% mutate(NUOVA_CLASSE = fct_explicit_na(NUOVA_CLASSE,"Clienti_persi"))
 
 matrice_classi_az=matrix(0,ncol = 7,nrow = 2)
 colnames(matrice_classi_az) <- c("Cheap","Tin","Copper","Bronze","Silver","Gold","Diamond")
@@ -339,13 +340,14 @@ Confronto_RFM_V_az <- Confronto_RFM_az %>% group_by(VECCHIA_CLASSE) %>% summaris
 Confronto_RFM_N_az <- Confronto_RFM_az %>% group_by(NUOVA_CLASSE) %>% summarise(CLI_TOT_nuovi = n()) %>% mutate(CLASSE = NUOVA_CLASSE) %>% select(- NUOVA_CLASSE)
 
 Confronto_RFM_TOT_az <- Confronto_RFM_V_az %>% right_join(Confronto_RFM_N_az, by="CLASSE")
+Confronto_RFM_TOT_az <- Confronto_RFM_TOT_az %>% filter(CLASSE != "Clienti_persi")
 
 matrice_classi_az[1,] <- c(Confronto_RFM_TOT_az$CLI_TOT_vecchi[1],Confronto_RFM_TOT_az$CLI_TOT_vecchi[2],Confronto_RFM_TOT_az$CLI_TOT_vecchi[3],Confronto_RFM_TOT_az$CLI_TOT_vecchi[4],Confronto_RFM_TOT_az$CLI_TOT_vecchi[5],Confronto_RFM_TOT_az$CLI_TOT_vecchi[6],Confronto_RFM_TOT_az$CLI_TOT_vecchi[7])
 matrice_classi_az[2,] <- c(Confronto_RFM_TOT_az$CLI_TOT_nuovi[1],Confronto_RFM_TOT_az$CLI_TOT_nuovi[2],Confronto_RFM_TOT_az$CLI_TOT_nuovi[3],Confronto_RFM_TOT_az$CLI_TOT_nuovi[4],Confronto_RFM_TOT_az$CLI_TOT_nuovi[5],Confronto_RFM_TOT_az$CLI_TOT_nuovi[6],Confronto_RFM_TOT_az$CLI_TOT_nuovi[7])
 df_matrice_classi_az <- as.data.frame(matrice_classi_az)
 
 
-Confronto_RFM2_az <- Confronto_RFM_az %>% filter(VECCHIA_CLASSE != "Clienti_futuri")
+Confronto_RFM2_az <- Confronto_RFM_az %>% filter(VECCHIA_CLASSE != "Clienti_futuri" & NUOVA_CLASSE != "Clienti_persi")
 
 Confronto_RFM3_az <- Confronto_RFM2_az %>% group_by(VECCHIA_CLASSE, NUOVA_CLASSE) %>% summarise(CONTO = n())
 
@@ -355,4 +357,9 @@ Nuovi_clientiRFm2_az <- Nuovi_clientiRFm_az %>% group_by(NUOVA_CLASSE) %>% summa
 Nuovi_clientiRFm2_az <- Nuovi_clientiRFm2_az %>% mutate(PERC = percent(COUNT/sum(COUNT)))
 
 #la percentuale dei nuovi clienti: meno del 50% fa parte della categoria Tin e Cheap, un 23% è Bronze e un 15% è Silver
+
+
+Persi_clientiRFm_az<- Confronto_RFM_az %>% filter(NUOVA_CLASSE == "Clienti_persi") %>% select(-NUOVA_CLASSE)
+Persi_clientiRFm2_az <- Persi_clientiRFm_az %>% group_by(VECCHIA_CLASSE) %>% summarise(COUNT = n())
+Persi_clientiRFm2_az <- Persi_clientiRFm2_az %>% mutate(PERC = percent(COUNT/sum(COUNT)))
 
